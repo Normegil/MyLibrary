438e092d0843eead55a241788cff2b180afdb83c
Olivier Ory <mail@normegil.be>
2014-11-18 15:06:59 +0100

    Add Test For constraint NotEmptyValidator

--- src/test/java/be/normegil/mylibrary/util/UtilPackageTestSuite.java
+++ src/test/java/be/normegil/mylibrary/util/UtilPackageTestSuite.java
@@ -1,11 +1,13 @@
 package be.normegil.mylibrary.util;
 
+import be.normegil.mylibrary.util.constraint.ConstraintPackageTestSuite;
 import be.normegil.mylibrary.util.dao.DAOPackageTestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+		ConstraintPackageTestSuite.class,
 		DAOPackageTestSuite.class
 })
 public class UtilPackageTestSuite {
---
+++ src/test/java/be/normegil/mylibrary/util/constraint/UTNotEmptyValidator.java
@@ -0,0 +1,30 @@
+package be.normegil.mylibrary.util.constraint;
+
+import org.hibernate.validator.internal.engine.constraintvalidation.ConstraintValidatorContextImpl;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class UTNotEmptyValidator {
+
+	private static final String EMPTY_STRING = "";
+	private static final String NOT_EMPTY_STRING = "Test";
+	private NotEmptyValidator entity = new NotEmptyValidator();
+	private ConstraintValidatorContextImpl context = new ConstraintValidatorContextImpl(null, null, null);
+
+	@Test
+	public void testNull() throws Exception {
+		assertFalse(entity.isValid(null, context));
+	}
+
+	@Test
+	public void testEmpty() throws Exception {
+		assertFalse(entity.isValid(EMPTY_STRING, context));
+	}
+
+	@Test
+	public void testNotEmpty() throws Exception {
+		assertTrue(entity.isValid(NOT_EMPTY_STRING, context));
+	}
+}
